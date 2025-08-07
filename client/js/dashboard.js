f4b13c5b07c622a0d1e11d1ead2fcd6d02411e68
Asiru Adedolapo <105151400+Don-pizu@users.noreply.github.com>
2025-08-07 11:54:07 +0100

    git commit -m "Session 50A: Frontend integration with backend

---
+++ client/js/dashboard.js
@@ -0,0 +1,50 @@
+// Set the backend API URL 
+// const API = 'http://localhost:5000/api'; // for testing
+
+const API = 'https://fintech-dashboard-2ifo.onrender.com/api';    //For production after deployment
+
+// Get token from localStorage 
+const token = localStorage.getItem('token'); 
+
+// Fetch dashboard data 
+async function loadDashboard() { 
+  const res = await fetch(`${API}/dashboard/dashboard`, { 
+    headers: { Authorization: `Bearer ${token}` } 
+  }); 
+  
+
+  const data = await res.json();
+
+   // Handle image display
+    const imageTag = data.profileImage
+      ? `<img src="http://localhost:5000/${data.profileImage}" width="150" />`
+      : '<p>No profile image uploaded</p>';
+
+  document.getElementById('dashboard').innerHTML = ` 
+    ${imageTag}
+    <p>Username: ${data.username}</p> 
+    <p>Role: ${data.role}</p> 
+    <p>Balance: $${data.balance}</p> 
+    <p>Total Transactions: ${data.totalTransaction}</p> 
+  `; 
+} 
+
+
+// Logout helper 
+function logout() { 
+  localStorage.removeItem('token'); 
+  window.location.href = 'login.html'; 
+} 
+
+
+// Wait for DOM to be ready
+document.addEventListener('DOMContentLoaded', () => {
+  const logoutBtn = document.getElementById('logoutBtn');
+  if (logoutBtn) {
+    logoutBtn.addEventListener('click', logout);
+  }
+});
+
+
+
+loadDashboard();
---
+++ client/js/main.js
@@ -0,0 +1,33 @@
+// Set the backend API URL 
+// const API = 'http://localhost:5000/api'; // for testing
+const API = 'https://fintech-dashboard-2ifo.onrender.com/api'; // For production
+
+document.getElementById("loginForm").addEventListener("submit", async function(e) {
+	e.preventDefault();
+
+	const email = document.getElementById("email").value;
+	const password = document.getElementById("password").value;
+
+	try {
+		const response = await fetch(`${API}/auth/login`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"  
+			},
+			body: JSON.stringify({ email, password }),
+		});
+
+		const data = await response.json();
+
+		if (response.ok) {
+			alert("Login successful");
+			localStorage.setItem("token", data.token);  // Save JWT
+			window.location.href = "dashboard.html";
+		} else {
+			alert(data.message || "Login failed");
+		}
+	} catch (err) {
+		console.error("Login error:", err);
+		alert("An error occurred during login.");
+	}
+});
